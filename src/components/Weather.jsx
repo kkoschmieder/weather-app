c3dbb9bdbf8d4ba7d4c50ad105ee97d54acf349a
kkoschmieder <karolkoschmieder@gmail.com>
2023-06-11 21:19:48 +0200

    fixed: weather icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,7 +4,7 @@ import iconmap from '../utils/handleIcon'
 function Weather(props) {
     return (
         <div className="weather">
-            <img src={'/images/weathericons/' + iconmap.get(props.data?.iconCode) + '.svg'} alt='' />
+            <img src={'./images/weathericons/' + iconmap.get(props.data?.iconCode) + '.svg'} alt='' />
             <p>{props.data?.currentTemperature} °C</p>
             <p>{props.data?.apparentTemperature} °C</p>
             <p>{props.data?.windSpeed} km/h</p>
